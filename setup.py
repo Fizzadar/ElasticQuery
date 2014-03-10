6d3e97fe1f8254610bc4aad79b489c0e4c091feb
Nick Barrett <nick@oxygem.com>
2014-03-10 16:20:33 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    version = '0.1.5',
+    version = '0.1.6',
     name = 'ElasticQuery',
     description = 'A simple query builder for Elasticsearch',
     author = 'Nick Barrett',
