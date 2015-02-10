9320691ed29b25fa8974c0df3eb9b477eaffe303
Nick Barrett <nick@oxygem.com>
2015-02-10 18:42:23 +0000

    Test filter & query string/raw_string

--- test.py
+++ test.py
@@ -31,7 +31,7 @@ def test(name, got, want):
     if got != want:
         print 'want: {0}'.format(json.dumps(want))
         print 'got: {0}'.format(json.dumps(got))
-        error('{0} fail: {1} != {2}'.format(name, got, want))
+        error('{0} fail'.format(name))
     else:
         success('{0}'.format(name))
 
@@ -78,47 +78,92 @@ FILTERS = {
             }
         }
     },
-    'RAW_STRING': {
+    'RAW_FILTER_STRING': {
+        'query': {
+            'query_string': {
+                'query': 'QUERYSTRING',
+                'default_operator': 'AND'
+            }
+        }
+    },
+    'RAW_QUERY_STRING': {
         'query_string': {
             'query': 'QUERYSTRING',
             'default_operator': 'AND'
         }
     },
-    'STRING': {
+    'FILTER_STRING': {
+        'query': {
+            'query_string': {
+                'query': 'field_name1:value_name1 AND (field_name2:((value_name2) OR (value_name3)))',
+                'default_operator': 'AND'
+            }
+        }
+    },
+    'QUERY_STRING': {
         'query_string': {
             'query': 'field_name1:value_name1 AND (field_name2:((value_name2) OR (value_name3)))',
             'default_operator': 'AND'
         }
     },
     'NESTED_FILTER': {
-
+        'nested': {
+            'path': 'nested_path',
+            'filter': {
+                'bool': {
+                    'must': [{
+                        'term': {
+                            'field_name1': 'value_name1'
+                        }
+                    }],
+                    'should': [],
+                    'must_not': []
+                }
+            }
+        }
     }
 }
 
 # Test filters
 print '[ElasticQuery] Testing: filters & queries'
-query = Query.range('field_name1', gt=0, lte=100)[1]
+
+query = Filter.range('field_name1', gt=0, lte=100)[2]
+test('Filter.range', query, FILTERS['RANGE'])
+query = Query.range('field_name1', gt=0, lte=100)[2]
 test('Query.range', query, FILTERS['RANGE'])
 
-query = Query.prefix(field_name1='value_name1')[1]
+query = Filter.prefix(field_name1='value_name1')[2]
+test('Filter.prefix', query, FILTERS['PREFIX'])
+query = Query.prefix(field_name1='value_name1')[2]
 test('Query.prefix', query, FILTERS['PREFIX'])
 
-query = Query.term(field_name1='value_name1')[1]
+query = Filter.term(field_name1='value_name1')[2]
+test('Filter.term', query, FILTERS['TERM'])
+query = Query.term(field_name1='value_name1')[2]
 test('Query.term', query, FILTERS['TERM'])
 
-query = Query.terms(field_name1=['value_name1', 'value_name2'])[1]
+query = Filter.terms(field_name1=['value_name1', 'value_name2'])[2]
+test('Filter.terms', query, FILTERS['TERMS'])
+query = Query.terms(field_name1=['value_name1', 'value_name2'])[2]
 test('Query.terms', query, FILTERS['TERMS'])
 
-query = Filter.missing('field_name1')[1]
+query = Filter.missing('field_name1')[2]
 test('Filter.missing', query, FILTERS['FILTER_MISSING'])
-query = Query.missing('field_name1')[1]
+query = Query.missing('field_name1')[2]
 test('Query.missing', query, FILTERS['QUERY_MISSING'])
 
-query = Query.raw_string('QUERYSTRING')[1]
-test('Query.raw_string', query, FILTERS['RAW_STRING'])
+query = Filter.raw_string('QUERYSTRING')[2]
+test('Filter.raw_string', query, FILTERS['RAW_FILTER_STRING'])
+query = Query.raw_string('QUERYSTRING')[2]
+test('Query.raw_string', query, FILTERS['RAW_QUERY_STRING'])
 
-query = Query.string(field_name1='value_name1', field_name2=['value_name2', 'value_name3'])[1]
-test('Query.string', query, FILTERS['STRING'])
+query = Filter.string(field_name1='value_name1', field_name2=['value_name2', 'value_name3'])[2]
+test('Filter.string', query, FILTERS['FILTER_STRING'])
+query = Query.string(field_name1='value_name1', field_name2=['value_name2', 'value_name3'])[2]
+test('Query.string', query, FILTERS['QUERY_STRING'])
+
+query = Filter.nested('nested_path', must=[Filter.term(field_name1='value_name1')])[2]
+test('Query.raw_string', query, FILTERS['NESTED_FILTER'])
 
 
 # Aggregates:
@@ -203,33 +248,33 @@ AGGREGATES = {
 # Test aggregates
 print '[ElasticQuery] Testing: aggregates'
 aggregate = Aggregate.stats('field_name1')
-test('Aggregate.stats', aggregate, AGGREGATES['STATS'])
+test('Aggregate.stats', aggregate[1], AGGREGATES['STATS'])
 
 aggregate = Aggregate.extended_stats('field_name1')
-test('Aggregate.extended_stats', aggregate, AGGREGATES['EXTENDED_STATS'])
+test('Aggregate.extended_stats', aggregate[1], AGGREGATES['EXTENDED_STATS'])
 
 aggregate = Aggregate.histogram('field_name1', interval=100)
-test('Aggregate.histogram', aggregate, AGGREGATES['HISTOGRAM'])
+test('Aggregate.histogram', aggregate[1], AGGREGATES['HISTOGRAM'])
 
 aggregate = Aggregate.date_histogram('field_name1')
-test('Aggregate.date_histogram', aggregate, AGGREGATES['DATE_HISTOGRAM'])
+test('Aggregate.date_histogram', aggregate[1], AGGREGATES['DATE_HISTOGRAM'])
 
 aggregate = Aggregate.terms('field_name1')
-test('Aggregate.terms', aggregate, AGGREGATES['TERMS'])
+test('Aggregate.terms', aggregate[1], AGGREGATES['TERMS'])
 
-aggregate = Aggregate.sub(Aggregate.terms('field_name1'), **{ 'sub_aggregate1': Aggregate.sum('sub_field1')})
-test('Aggregate.sum + sub aggregate', aggregate, AGGREGATES['SUB_AGGREGATES'])
+aggregate = Aggregate.sub(Aggregate.terms('field_name1'), **{'sub_aggregate1': Aggregate.sum('sub_field1')})
+test('Aggregate.sum + sub aggregate', aggregate[1], AGGREGATES['SUB_AGGREGATES'])
 
 aggregate = Aggregate.sub(
-    Aggregate.filter(musts=[Filter.term(**{ 'key': 'value' })]), **{
+    Aggregate.filter(must=[Filter.term(**{'key': 'value'})]), **{
         'price_histogram': Aggregate.sub(
-            Aggregate.terms('bp_now'),**{
-                'option_count':Aggregate.sum('option_count_current')
+            Aggregate.terms('bp_now'), **{
+                'option_count': Aggregate.sum('option_count_current')
             }
-       )
+        )
     }
 )
-test('Aggregate.filter + sub aggregate', aggregate, AGGREGATES['FILTER_SUB_AGGREGATES'])
+test('Aggregate.filter + sub aggregate', aggregate[1], AGGREGATES['FILTER_SUB_AGGREGATES'])
 
 # Queries
 QUERIES = {
